ec6481fe46ba7d7a3a2417977507d202f024ce08
imdiode <nandanparagshukla@gmail.com>
2021-03-13 09:05:32 +0530

    Profile page UI complete

---
+++ Backend/JS/ProfileUI.js
@@ -0,0 +1,11 @@
+function disp ()
+{
+    if (document.getElementById("1exp").style.display == "block")
+    {
+        document.getElementById("1exp").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("1exp").style.display = "block";
+    }
+}
