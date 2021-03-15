095329d2a35064f18a96faba9db8660ade3d7dc2
Mohit Shah <mohitshah.ict19@gmail.com>
2021-03-15 15:01:49 +0530

    ui updated

--- Backend/JS/supportandfaq.js
+++ Backend/JS/supportandfaq.js
@@ -22,3 +22,38 @@ function dispcontactus ()
         document.getElementById("contactus").style.display = "block";
     }
 }
+
+function dispans1 ()
+{
+    if (document.getElementById("ans1").style.display == "block")
+    {
+        document.getElementById("ans1").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("ans1").style.display = "block";
+    }
+}
+
+function dispans2 ()
+{
+    if (document.getElementById("ans2").style.display == "block")
+    {
+        document.getElementById("ans2").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("ans2").style.display = "block";
+    }
+}
+function dispans3 ()
+{
+    if (document.getElementById("ans3").style.display == "block")
+    {
+        document.getElementById("ans3").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("ans3").style.display = "block";
+    }
+}
