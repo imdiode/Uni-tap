d8080ebc025c328c2279ec2ef4e82816e4c279fd
Mohit Shah <mohitshah.ict19@gmail.com>
2021-03-15 14:14:25 +0530

    UI updation

---
+++ Backend/JS/supportandfaq.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+function dispfaq ()
+{
+    if (document.getElementById("faq").style.display == "block")
+    {
+        document.getElementById("faq").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("faq").style.display = "block";
+    }
+}
+
+function dispcontactus ()
+{
+    if (document.getElementById("contactus").style.display == "block")
+    {
+        document.getElementById("contactus").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("contactus").style.display = "block";
+    }
+}
