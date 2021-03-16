6922ad2ca9d27c844004c1609884f862c1063d84
imdiode <nandanparagshukla@gmail.com>
2021-03-16 16:44:40 +0530

    link updation

---
+++ Backend/JS/redirect.js
@@ -0,0 +1,14 @@
+function profile_click()
+{
+    location.href = "Profile.html";
+}
+
+function sports_click()
+{
+    location.href = "sports.html";
+}
+
+function canteen_click()
+{
+    location.href = "canteen.html";
+}
