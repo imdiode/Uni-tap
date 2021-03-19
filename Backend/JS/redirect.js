18e2cabf35a58a67368772136d85c323bef755b5
imdiode <nandanparagshukla@gmail.com>
2021-03-19 13:16:27 +0530

    all pages linked

--- Backend/JS/redirect.js
+++ Backend/JS/redirect.js
@@ -3,6 +3,26 @@ function profile_click()
     location.href = "Profile.html";
 }
 
+function library_click()
+{
+    location.href = "library.html";
+}
+
+function help_click()
+{
+    location.href = "help_desk.html";
+}
+
+function griv_click()
+{
+    location.href = "grievance.html";
+}
+
+function aca_click()
+{
+    location.href = "academic_records.html";
+}
+
 function sports_click()
 {
     location.href = "sports.html";
