a50aecce29f7fd1b29f980e88572a3420869af56
imdiode <nandanparagshukla@gmail.com>
2021-06-27 16:06:29 +0530

    File upload working for images, changing to csv left

---
+++ Backend/JS/profile_upload.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', function() {
+
+    let file;
+
+    const dropArea = document.querySelector(".drag-container"),
+        dragText = dropArea.querySelector("header"),
+        button = dropArea.querySelector("button"),
+        input = dropArea.querySelector("input");
+
+
+    button.onclick = () => {
+        input.click();
+    }
+
+    input.addEventListener("change", function() {
+        file = this.files[0];
+        dropArea.classList.add("active");
+        showFile();
+    });
+
+
+    dropArea.addEventListener("dragover", (event) => {
+        event.preventDefault();
+        dropArea.classList.add("active");
+        dragText.textContent = "Release to Profile File";
+    });
+
+    dropArea.addEventListener("dragleave", () => {
+        dropArea.classList.remove("active");
+        dragText.textContent = "Drag & Drop to Profile File";
+    });
+
+    dropArea.addEventListener("drop", (event) => {
+        event.preventDefault();
+        file = event.dataTransfer.files[0];
+        showFile();
+    });
+
+    function showFile() {
+        let fileType = file.type;
+        let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+        if (validExtensions.includes(fileType)) {
+            dropArea.classList.remove("active");
+            dragText.textContent = "Profile File Selected";
+        } else {
+            alert("This is not an CSV File!");
+            dropArea.classList.remove("active");
+            dragText.textContent = "Drag & Drop to Profile File";
+        }
+    }
+});
