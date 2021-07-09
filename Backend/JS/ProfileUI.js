956f51dfb4c5bdd0371eef5292f8fb86bba132c6
enculandus <aniket120301@gmail.com>
2021-07-09 22:28:16 +0530

    profile page dynamic updating enabled

--- Backend/JS/ProfileUI.js
+++ Backend/JS/ProfileUI.js
@@ -1,3 +1,26 @@
+window.onload = loadProfile();
+
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const profileRef = db.collection('users').doc(auth.currentUser.uid);
+
+async function loadProfile() {
+  profileRef.get().then((doc)=>{
+    const profileData = doc.data();
+    document.getElementById("navFirstName").innerHTML = profileData.firstName;
+    document.getElementById("fullName").innerHTML = profileData.firstName + " " + profileData.lastName;
+    document.getElementById("semester").innerHTML = profileData.semester;
+    document.getElementById("emailId").innerHTML = profileData.emailId;
+    document.getElementById("moblieNumber").innerHTML = profileData.mobileNumber;
+    document.getElementById("address").innerHTML = "<td>" + profileData.address.line1 + "<br>" + "</td>";
+    document.getElementById("studentUID").innerHTML = profileData.studentUID;
+  }).catch((err)=>{
+    console.log(err);
+  })
+}
+
+
 function disp ()
 {
     if (document.getElementById("1exp").style.display == "block")
