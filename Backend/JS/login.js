1068d03e29b94772fd5e49d03d8eea2aee24c935
imdiode <nandanparagshukla@gmail.com>
2021-07-11 12:04:01 +0530

    minor profile changes and login loader changes

--- Backend/JS/login.js
+++ Backend/JS/login.js
@@ -7,7 +7,7 @@ const fadeEffect = setInterval(() => {
     if (preloader.style.opacity > 0) {
         preloader.style.opacity -= 0.1;
     } else {
-        preloader.style.zIndex = 0;
+        preloader.style.display = 'none';
         clearInterval(fadeEffect);
     }
 }, 200);
@@ -27,15 +27,15 @@ document.getElementById('passw').addEventListener("keyup", function(event) {
 
 //to check if login is already present.
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    location.href = "Dashboard.html";
-    //console.log(uid);
-    //loadProfile();
-    // ...
-  }
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        location.href = "Dashboard.html";
+        //console.log(uid);
+        //loadProfile();
+        // ...
+    }
 });
 
 function red() {
