ccd0bb318eff2827ec90b358772edee7d8fa92f0
imdiode <nandanparagshukla@gmail.com>
2021-07-11 11:53:43 +0530

    pre-loader change, profile change partial

--- Backend/JS/login.js
+++ Backend/JS/login.js
@@ -1,13 +1,13 @@
 const preloader = document.querySelector('.loader');
 
 const fadeEffect = setInterval(() => {
-    // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
     if (!preloader.style.opacity) {
         preloader.style.opacity = 1;
     }
     if (preloader.style.opacity > 0) {
         preloader.style.opacity -= 0.1;
     } else {
+        preloader.style.zIndex = 0;
         clearInterval(fadeEffect);
     }
 }, 200);
