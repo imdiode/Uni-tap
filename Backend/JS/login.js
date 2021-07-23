d4d40101c12d18ada11fb48116247612e8762449
imdiode <nandanparagshukla@gmail.com>
2021-07-23 07:45:43 +0530

    preloader brought further forward

--- Backend/JS/login.js
+++ Backend/JS/login.js
@@ -24,6 +24,8 @@ document.getElementById('passw').addEventListener("keyup", function(event) {
     if (event.key === 'Enter') {
         // Cancel the default action, if needed
         event.preventDefault();
+        preloader.style.display = 'flex';
+        preloader.style.opacity += 1;
         // Trigger the button element with a click
         red();
     }
@@ -35,8 +37,6 @@ firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        preloader.style.opacity += 1;
-        preloader.style.display = 'block';
         var uid = user.uid;
         location.href = "Dashboard.html";
         //console.log(uid);
