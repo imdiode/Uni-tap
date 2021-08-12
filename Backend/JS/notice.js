14ab7095dbf9bc7dde4f079f8fef1c93268df206
enculandus <aniket120301@gmail.com>
2021-08-12 11:49:00 +0530

    try 3

---
+++ Backend/JS/notice.js
@@ -0,0 +1,52 @@
+/* ______________________________By @Enculandus______________________________ */
+/* __________________________________________________________________________ */
+
+
+var db;
+var rtdb;
+var auth;
+var storage;
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    //console.log(uid);
+    //initializePage();
+    // ...
+    db = firebase.firestore();
+    rtdb = firebase.database();
+    auth = firebase.auth();
+    storage = firebase.storage();
+    initNotices();
+  } else {
+    // User is signed out
+    // ...
+    location.href = "index.html";
+  }
+});
+
+async function initNotices() {
+  let noticesRef = rtdb.ref("notices");
+  noticesRef.get().then((snap)=>{
+    snap.forEach((childSnap)=>{
+      let dat = childSnap.val();
+      let card = "<div class=\"notice\"><center>"+ dat.noticeUID +"</center><a href="+ dat.downloadLink +"> Download</a></div>";
+      let dom = new DOMParser().parseFromString(card,'text/html');
+      let card_element = dom.body.firstElementChild;
+      document.getElementById('mainNoticeBoard').append(card_element);
+    }).then((res)=>{
+      noticesRef.on('child_added', (data)=>{
+        let dat = data.val();
+        let card = "<div class=\"notice\"><center>"+ dat.noticeUID +"</center><a href="+ dat.downloadLink +"> Download</a></div>";
+        let dom = new DOMParser().parseFromString(card,'text/html');
+        let card_element = dom.body.firstElementChild;
+        document.getElementById('mainNoticeBoard').append(card_element);
+      })
+    })
+  });
+}
+
+/* __________________________________________________________________________ */
+/* __________________________________________________________________________ */
