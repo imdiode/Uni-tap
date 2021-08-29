91df881911f0b237c57f42fc99ae905a1729831b
Mann-tech13 <mannshah.ict19@gmail.com>
2021-08-29 12:44:34 +0530

    ProfilePage-btnAdd

--- Backend/JS/chat-app.js
+++ Backend/JS/chat-app.js
@@ -1,16 +1,5 @@
 const button_toadd = document.getElementsByClassName("add");
 
-// function open_page() {
-//     const lst_users = document.getElementById("new_user_list");
-//     lst_users.classList.add("show")
-//     shownewchatlist();
-// }
-
-// function close_page() {
-//     const lst_users = document.getElementById("new_user_list");
-//     lst_users.classList.remove("show")
-// }
-
 function chatStarted() {
     const chatList = document.querySelector("#chatList");
     const message_workspace = document.querySelector(".message-workspace");
