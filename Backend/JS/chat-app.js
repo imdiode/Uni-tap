1eb56ddfde9a0115ff12952826b32c989a9c03fd
imdiode <nandanparagshukla@gmail.com>
2021-08-30 12:40:51 +0530

    chat-list UI final

--- Backend/JS/chat-app.js
+++ Backend/JS/chat-app.js
@@ -14,22 +14,14 @@ function chatStarted() {
 
 function open_page_add() {
     const lst_users = document.getElementById("new_user_list");
-    const chat_list = document.getElementById("chat_list");
-    const chat_panel = document.getElementById("chat-panel");
 
     lst_users.classList.add("show");
-    chat_list.style.opacity = "0.1";
-    chat_panel.style.opacity = "0.3";
-        // lst_users.style.filter = "blur(0px) !important"
+    // lst_users.style.filter = "blur(0px) !important"
 }
 
 function close_page_add() {
     const lst_users = document.getElementById("new_user_list");
-    const chat_list = document.getElementById("chat_list");
-    const chat_panel = document.getElementById("chat-panel");
 
     lst_users.classList.remove("show");
-    chat_list.style.opacity = "1";
-    chat_panel.style.opacity = "1";
     //shownewchatlist();
 }
