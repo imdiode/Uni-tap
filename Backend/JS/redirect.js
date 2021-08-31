4bfa4fdc0935504549e6896af0f297183f012990
imdiode <nandanparagshukla@gmail.com>
2021-08-31 11:39:53 +0530

    testing showname

--- Backend/JS/redirect.js
+++ Backend/JS/redirect.js
@@ -7,73 +7,66 @@ var profileRef;
 var storage;
 
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    //console.log(uid);
-    //initializePage();
-    // ...
-    db = firebase.firestore();
-    auth = firebase.auth();
-    storage = firebase.storage();
-  
-  } else {
-    // User is signed out
-    // ...
-    location.href = "index.html";
-  }
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        //console.log(uid);
+        //initializePage();
+        // ...
+        db = firebase.firestore();
+        auth = firebase.auth();
+        storage = firebase.storage();
+
+    } else {
+        // User is signed out
+        // ...
+        location.href = "index.html";
+    }
 });
 
 async function showName() {
-  let name = auth.currentUser.displayName.split(" ");
-  document.getElementById("navFirstName").innerHTML = name[0];
+    let name = auth.currentUser.displayName.split(" ");
+    document.getElementById("navFirstName").innerHTML = name[0];
 }
+showName();
 
 async function firebaseLogout() {
-  firebase.auth().signOut()
-    .then((ret)=>{
-      location.href = "index.html";
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
+    firebase.auth().signOut()
+        .then((ret) => {
+            location.href = "index.html";
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
-
 /* __________________________________________________________________________ */
 /* __________________________________________________________________________ */
 
-function profile_click()
-{
+function profile_click() {
     location.href = "Profile.html";
 }
 
-function library_click()
-{
+function library_click() {
     location.href = "library.html";
 }
 
-function help_click()
-{
+function help_click() {
     location.href = "help_desk.html";
 }
 
-function griv_click()
-{
+function griv_click() {
     location.href = "grievance.html";
 }
 
-function aca_click()
-{
+function aca_click() {
     location.href = "academic_records.html";
 }
 
-function sports_click()
-{
+function sports_click() {
     location.href = "sports.html";
 }
 
-function canteen_click()
-{
+function canteen_click() {
     location.href = "canteen.html";
 }
